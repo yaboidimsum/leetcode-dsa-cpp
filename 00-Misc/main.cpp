39176cd12c41ce0c03ed765efe63fe18b0ec2df1
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-09-29 14:50:34 +0700

    feat: add misc and bigO

---
+++ 00-Misc/main.cpp
@@ -0,0 +1,11 @@
+/*This MISC only for testing if the C++ Working. The code will be written sometimes in the future */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
