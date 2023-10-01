e54353dd7b70e2b98398d7867d81b8f01203a987
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-10-01 13:25:03 +0700

    feat: add exercise

--- 03-LinkedList/main.cpp
+++ 03-LinkedList/main.cpp
@@ -26,8 +26,60 @@ class LinkedList
             tail = newNode;
             length = 1;
         }
+
+        ~LinkedList(){
+            Node *temp = head;
+            while(head){
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+    void append(int value){
+        Node *newNode = new Node(value);
+        if (length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail->next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    void printList(){
+        Node *temp = head;
+        while(temp){
+            cout << temp->value << endl;
+            temp = temp->next;
+        }
+    }
+
+    void getHead(){
+        if (head == nullptr){
+            cout << "Head: nullptr" << endl;
+        } else {
+            cout << "Head: " << head->value << endl;
+        }
+    }
+
+    void getTail(){
+        if(tail == nullptr){
+            cout << "Tail: nullptr" << endl;
+        } else {
+            cout << "Tail: " << tail->value << endl;
+        }
+    }
+
+    void getLength(){
+        cout << "Length: " << length << endl;
+    }
 };
 
 int main(){
-    LinkedList *myLinkedList = new LinkedList(10);
+    LinkedList *myLinkedList = new LinkedList(1);
+    myLinkedList->append(2);
+
+    myLinkedList->printList();
 }
