f629c05cb785bca99c81cb5368a57b2ad3240513
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-10-17 12:56:41 +0700

    feat: add PartitionList

---
+++ 03-LinkedList/LL-Interview/EXERCISE-LL-PartitionList.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    private:
+        Node* head;
+        int length;
+        
+    public:
+        LinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            length = 1;
+        }
+
+        ~LinkedList() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        void printList() {
+            Node* temp = head;
+            if (temp == nullptr) {
+                cout << "empty";
+            } else {
+                while (temp != nullptr) {
+                    cout << temp->value;
+                    temp = temp->next;
+                    if (temp != nullptr) {
+                        cout << " -> ";
+                    }
+                }
+            }
+            cout << endl;
+        }
+
+        Node* getHead() {
+            return head;
+        }
+
+        int getLength() {
+            return length;
+        }
+        
+        void makeEmpty() {
+            Node* temp = head;
+            while (head) {
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+            length = 0;
+        }
+
+        void append(int value) {
+            Node* newNode = new Node(value);
+            if (head == nullptr) {
+                head = newNode;
+            } else {
+                Node* currentNode = head;
+                while (currentNode->next != nullptr) {
+                    currentNode = currentNode->next;
+                }
+                currentNode->next = newNode;
+            }
+            length++;
+        }
+
+        //   +======================================================+
+        //   |                 WRITE YOUR CODE HERE                 |
+        //   | Description:                                         |
+        //   | - Partition list around value x                      |
+        //   | - Return type: void                                  |
+        //   |                                                      |
+        //   | Tips:                                                |
+        //   | - Create two dummy nodes for two new lists           |
+        //   | - One list for nodes less than x                     |
+        //   | - Another list for nodes greater or equal to x       |
+        //   | - Loop through original list                         |
+        //   | - Assign nodes to new lists based on value           |
+        //   | - Merge the two new lists                            |
+        //   | - Update the original list's head                    |
+        //   +======================================================+
+        void partitionList(int x){
+            Node* lessThanX = new Node(0);
+            Node* greaterThanX = new Node(0);
+            Node* lessThanXHead = lessThanX;
+            Node* greaterThanXHead = greaterThanX;
+            Node* currentNode = head;
+            
+            while(currentNode != nullptr){
+                if (currentNode->value < x){
+                    lessThanX->next = currentNode;
+                    lessThanX = lessThanX->next;
+                } else {
+                    greaterThanX->next = currentNode;
+                    greaterThanX = greaterThanX->next;
+                }
+                currentNode = currentNode->next;
+            }
+            lessThanX->next = greaterThanXHead->next;
+            greaterThanX->next = nullptr;
+            head = lessThanXHead->next;
+        
+        }
+
+};
+
+
