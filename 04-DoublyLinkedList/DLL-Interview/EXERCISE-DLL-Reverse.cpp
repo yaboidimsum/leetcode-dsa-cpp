66825148bb341fa6ad9a3e21d6e4fac429d23217
dimsum <101172637+yaboidimsum@users.noreply.github.com>
2023-10-24 21:03:51 +0700

    feat:add reverse dll

---
+++ 04-DoublyLinkedList/DLL-Interview/EXERCISE-DLL-Reverse.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class DoublyLinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    DoublyLinkedList(int value)
+    {
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    ~DoublyLinkedList()
+    {
+        Node *temp = head;
+        while (head)
+        {
+            head = head->next;
+            delete temp;
+            temp = head;
+        }
+    }
+
+    void printList()
+    {
+        Node *temp = head;
+        if (temp == nullptr)
+        {
+            cout << "empty" << endl;
+            return;
+        }
+        while (temp->next != nullptr)
+        {
+            cout << temp->value << " <-> ";
+            temp = temp->next;
+        }
+        cout << temp->value << endl;
+    }
+
+    Node *getHead()
+    {
+        return head;
+    }
+
+    Node *getTail()
+    {
+        return tail;
+    }
+
+    int getLength()
+    {
+        return length;
+    }
+
+    void append(int value)
+    {
+        Node *newNode = new Node(value);
+        if (length == 0)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    //   +=====================================================+
+    //   |                 WRITE YOUR CODE HERE                |
+    //   | Description:                                        |
+    //   | - This is the reverse method.                       |
+    //   | - It reverses the entire doubly linked list.        |
+    //   | - Return type: void                                 |
+    //   |                                                     |
+    //   | Tips:                                               |
+    //   | - Create two pointers: current and temp.            |
+    //   | - Loop through the list swapping next and prev      |
+    //   |   for each node.                                    |
+    //   | - After loop, swap head and tail pointers.          |
+    //   | - Check output from Test.cpp in "User logs".        |
+    //   +=====================================================+
+
+    void reverse()
+    {
+        Node *current = head;
+        Node *temp = nullptr;
+        while (current != nullptr)
+        {
+            temp = current->prev;
+            current->prev = current->next;
+            current->next = temp;
+            current = current->prev;
+        }
+        temp = head;
+        head = tail;
+        tail = temp;
+    }
+};
